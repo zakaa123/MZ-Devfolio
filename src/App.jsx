845506c6b83a2e6d814eb9f2b072554eb3439d71
zaka <muhammadzakaa82@gmail.com>
2025-08-16 23:18:18 +0500

    Fix: corrected styles path case for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "./styles/main.css";
+import "./Styles/main.css";
 
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
